af32fd4c47a621e93cfa7e52393ad555390c9b15
Damian O <osieckidamian1992@gmail.com>
2020-03-06 19:42:38 +0100

    hello

--- start.py
+++ start.py
@@ -1,1 +1,31 @@
-print("Hello")
+a=4
+b=2
+
+odejmowanie=a-b
+dodawanie=a+b
+mnozenie=a*b
+dzielenie=a/b
+print(odejmowanie)
+print(dodawanie)
+print(mnozenie)
+print(dzielenie)
+
+print("ZAD 2")
+a**=b
+print(a)
+a+=b
+print(a)
+a-=b
+print(a)
+a/=b
+print(a)
+a*=b
+print(a)
+a%=b
+print(a)
+from math import *
+print(e**10)
+sin=math.sinh(8)**2
+#x=math.log10(5+math.sin(8)**2)
+
+print(pow(x,1/6))
